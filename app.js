0f68af1649d8342d4763f31b2f2f03c8b09d3cce
MrException <robmcbride@gmail.com>
2011-04-06 08:32:49 -0700

    just added logger middleware

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
